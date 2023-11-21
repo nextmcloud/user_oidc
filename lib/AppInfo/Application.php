4150f620b85e95738034eec61a0b19ec91b8a511
Bernd.Rederlechner@t-systems.com <bernd.rederlechner@t-systems.com>
2023-11-21 12:26:26 +0100

    Customize ClientFlow, skip authpicker, late consent

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -48,6 +48,10 @@ use OCP\IUserSession;
 use Throwable;
 use Psr\Container\ContainerInterface;
 
+// this is needed only for the special, shortened client login flow
+use OCP\Security\ISecureRandom;
+use OCP\ISession;
+
 class Application extends App implements IBootstrap {
 	public const APP_ID = 'user_oidc';
 	public const OIDC_API_REQ_HEADER = 'Authorization';
@@ -90,7 +94,68 @@ class Application extends App implements IBootstrap {
 		try {
 			$context->injectFn(\Closure::fromCallable([$this, 'registerRedirect']));
 			$context->injectFn(\Closure::fromCallable([$this, 'registerLogin']));
+			// this is the custom auto-redirect for MagentaCLOUD client access
+			$context->injectFn(\Closure::fromCallable([$this, 'registerNmcClientFlow']));
 		} catch (Throwable $e) {
+		}
+	}
+
+	/**
+	 * This is the automatic redirect exclusively for Nextcloud/Magentacloud clients
+	 * completely skipping consent layer
+	 */
+	private function registerNmcClientFlow(IRequest $request, 
+            IURLGenerator $urlGenerator,
+            ProviderMapper $providerMapper, 
+            ISession $session, 
+            ISecureRandom $random): void {
+		
+        $providers = $this->getCachedProviders($providerMapper);
+
+		// Handle immediate redirect on client first-time login
+		$isClientLoginFlow = false;
+		try {
+			$isClientLoginFlow = $request->getPathInfo() === '/login/flow';
+		} catch (Exception $e) {
+			// in case any errors happen when checking for the path do not apply redirect logic as it is only needed for the login
+		}
+		if ($isClientLoginFlow) {
+			// only redirect if Telekom provider registered
+			$tproviders = array_values(array_filter($providers, function ($p) {
+				return strtolower($p->getIdentifier()) === "telekom";
+			}));
+			if (count($tproviders) == 0) {
+                // always show normal login flow as error fallback
+                return;
+            }
+
+            $stateToken = $random->generate(
+                64,
+                ISecureRandom::CHAR_LOWER.ISecureRandom::CHAR_UPPER.ISecureRandom::CHAR_DIGITS
+            );
+            $session->set('client.flow.state.token', $stateToken);
+ 
+            // call the service to get the params, but suppress the template
+            // compute grant redirect Url to go directly to Telekom login
+            $redirectUrl = $urlGenerator->linkToRoute('core.ClientFlowLogin.grantPage', [
+                'stateToken' => $stateToken,
+                // grantPage service operation is deriving oauth2 client name (again),
+                // so we simply pass on clientIdentifier or empty string 
+                'clientIdentifier' => $request->getParam('clientIdentifier', ''),
+                'direct' => $request->getParam('direct', '0')
+            ]);
+            if ($redirectUrl === null) {
+                // always show normal login flow as error fallback
+                return;
+            }
+
+            // direct login, consent layer later
+			$targetUrl = $urlGenerator->linkToRoute(self::APP_ID . '.login.login', [
+			    'providerId' => $tproviders[0]->getId(),
+				'redirectUrl' => $redirectUrl
+			]);
+			header('Location: ' . $targetUrl);
+			exit();
 		}
 	}
 
