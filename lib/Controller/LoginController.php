217b6474406f48d16851df3cd5f5b5e482989b50
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-02-04 10:51:04 +0100

    fix error

--- lib/Controller/LoginController.php
+++ lib/Controller/LoginController.php
@@ -540,17 +540,6 @@ class LoginController extends BaseOidcController {
 			$this->eventDispatcher->dispatchTyped(new UserLoggedInEvent($user, $user->getUID(), null, false));
 		}
 
-		// $tokenExchangeEnabled = (isset($oidcSystemConfig['token_exchange']) && $oidcSystemConfig['token_exchange'] === true);
-		// if ($tokenExchangeEnabled) {
-		// store all token information for potential token exchange requests
-		// $tokenData = array_merge(
-		// $data,
-		// ['provider_id' => $providerId],
-		// );
-		// $this->tokenService->storeToken($tokenData);
-		// }
-		// $this->config->setUserValue($user->getUID(), Application::APP_ID, 'had_token_once', '1');
-
 		// Set last password confirm to the future as we don't have passwords to confirm against with SSO
 		$this->session->set('last-password-confirm', strtotime('+4 year', time()));
 
